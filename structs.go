c0f650adb4eda991fd9472bc098c67aef5779de0
myathtut-zafir <aa.htut@gmail.com>
2025-10-24 23:15:15 +0700

    feat: using struct construtor method.

--- structs.go
+++ structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -20,17 +21,35 @@ func (appUser *user) clearUserInfo() {
 	appUser.lastName = ""
 	appUser.birthdate = ""
 }
+
+// util function to create a new user
+func newUser(firstName, lastName, birthdate string) (*user, error) {
+	if firstName == "" || lastName == "" {
+		return nil, errors.New("first name and last name cannot be empty")
+	}
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
 	// var appUser user
-	appUser := user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthdate: birthdate,
-		createdAt: time.Now(),
+	// appUser := user{
+	// 	firstName: firstName,
+	// 	lastName:  lastName,
+	// 	birthdate: birthdate,
+	// 	createdAt: time.Now(),
+	// }
+	appUser, error := newUser(firstName, lastName, birthdate)
+	if error != nil {
+		fmt.Println("Error creating user:", error)
+		return
 	}
 	appUser.printUserDetail()
 	appUser.clearUserInfo()
@@ -53,6 +72,6 @@ func printUserPointer(appUser *user) {
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
