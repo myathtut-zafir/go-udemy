e0753a61ede367f651a7c486a30290b5561890db
myathtut-zafir <aa.htut@gmail.com>
2025-10-24 21:48:29 +0700

    feat: test pointer

--- pointers.go
+++ pointers.go
@@ -6,11 +6,17 @@ func main() {
 	age := 30
 	agePointer := &age
 
-	fmt.Println("Value of age:", agePointer)
-	fmt.Println("Value of age value:", *agePointer)
-	fmt.Println("Value of age2:", updateAge(age))
+	// fmt.Println("Value of age:", agePointer)
+	// fmt.Println("Value of age value:", *agePointer)
+	// fmt.Println("Value of age2:", updateAge(agePointer))
+	fmt.Println("Value of age:", age)
+	updateAge2(agePointer)
+	fmt.Println("Value of age after updateAge2:", age)
 }
 
-func updateAge(age int) int {
-	return age - 18
+func updateAge(age *int) int {
+	return *age - 18
+}
+func updateAge2(age *int) {
+	*age = *age - 18
 }
